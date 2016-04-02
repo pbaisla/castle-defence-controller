eced3d79ac4a7db2c9af6d56a21a9923e5b32cc9
Prashant Baisla <prashantbaisla@gmail.com>
2016-04-02 20:11:18 +0530

    Add longclick listeners

--- app/src/main/java/com/example/prashant/castledefencecontroller/ControllerActivity.java
+++ app/src/main/java/com/example/prashant/castledefencecontroller/ControllerActivity.java
@@ -4,11 +4,14 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.HapticFeedbackConstants;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class ControllerActivity extends AppCompatActivity {
     Handler connectHandler = ReadyActivity.connectHandler;
@@ -19,7 +22,124 @@ public class ControllerActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_controller);
+        Button shoot_button = (Button) findViewById(R.id.shoot);
+        shoot_button.setOnTouchListener(new View.OnTouchListener() {
+
+            private Handler mHandler;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        if (mHandler != null) return true;
+                        mHandler = new Handler();
+                        mHandler.postDelayed(mAction, 500);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if (mHandler == null) return true;
+                        mHandler.removeCallbacks(mAction);
+                        mHandler = null;
+                        break;
+                }
+                return false;
+            }
+
+            Runnable mAction = new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Performing action...");
+                    if (connectHandler != null) {
+                        Message msg = connectHandler.obtainMessage();
+                        Bundle bundle = new Bundle();
+                        bundle.putString("move", "Shoot");
+                        msg.setData(bundle);
+                        connectHandler.sendMessage(msg);
+                    }
+                    mHandler.postDelayed(this, 500);
+                }
+            };
+
+        });
+
+        Button left_button = (Button) findViewById(R.id.left);
+        left_button.setOnTouchListener(new View.OnTouchListener() {
+
+            private Handler mHandler;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        if (mHandler != null) return true;
+                        mHandler = new Handler();
+                        mHandler.postDelayed(mAction, 500);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if (mHandler == null) return true;
+                        mHandler.removeCallbacks(mAction);
+                        mHandler = null;
+                        break;
+                }
+                return false;
+            }
+
+            Runnable mAction = new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Performing action...");
+                    if (connectHandler != null) {
+                        Message msg = connectHandler.obtainMessage();
+                        Bundle bundle = new Bundle();
+                        bundle.putString("move", "Left");
+                        msg.setData(bundle);
+                        connectHandler.sendMessage(msg);
+                    }
+                    mHandler.postDelayed(this, 500);
+                }
+            };
+
+        });
+
+        Button right_button = (Button) findViewById(R.id.right);
+        right_button.setOnTouchListener(new View.OnTouchListener() {
+
+            private Handler mHandler;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        if (mHandler != null) return true;
+                        mHandler = new Handler();
+                        mHandler.postDelayed(mAction, 500);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        if (mHandler == null) return true;
+                        mHandler.removeCallbacks(mAction);
+                        mHandler = null;
+                        break;
+                }
+                return false;
+            }
+
+            Runnable mAction = new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("Performing action...");
+                    if (connectHandler != null) {
+                        Message msg = connectHandler.obtainMessage();
+                        Bundle bundle = new Bundle();
+                        bundle.putString("move", "Right");
+                        msg.setData(bundle);
+                        connectHandler.sendMessage(msg);
+                    }
+                    mHandler.postDelayed(this, 500);
+                }
+            };
+
+        });
     }
+
     public void moveleft(View v) {
         if (connectHandler != null) {
             Message msg = connectHandler.obtainMessage();
@@ -47,4 +167,5 @@ public class ControllerActivity extends AppCompatActivity {
             connectHandler.sendMessage(msg);
         }
     }
+
 }
