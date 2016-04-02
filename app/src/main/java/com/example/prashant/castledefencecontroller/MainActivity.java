e6c716ce254bc27b5875bf9e05a1059a38936bb9
shinora007 <diablo7271@gmail.com>
2016-04-02 06:10:35 +0530

    creating first activity

--- app/src/main/java/com/example/prashant/castledefencecontroller/MainActivity.java
+++ app/src/main/java/com/example/prashant/castledefencecontroller/MainActivity.java
@@ -4,13 +4,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String ip_address = null;
+    private Button bt;
+    private EditText edTxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        bt = (Button) findViewById(R.id.button);
+        edTxt = (EditText) findViewById(R.id.editText);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), edTxt.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+        edTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edTxt.setText("");
+            }
+        });
     }
 
     @Override
